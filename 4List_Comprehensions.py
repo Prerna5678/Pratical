ba8fcd3795d196b853ba4d7d2473102bae72100d
Prerna5678 <2002jain2811@gmail.com>
2025-08-10 21:30:46 +0530

    Python

---
+++ 4List_Comprehensions.py
@@ -0,0 +1,3 @@
+# Creates a list of numbers from 0 to 9
+a = [i for i in range(10)]
+print(a)
---
+++ teacher.py
@@ -0,0 +1,19 @@
+'''
+Teacher
+1. Login
+2. Register
+0. Exit
+'''
+
+# Registration
+teacher=[]
+a=input("Enter your name")
+b=int(input("Enter your password"))
+
+for i in a:
+    teacher.append(a)
+print(teacher)
+
+for i in b:
+    teacher.append(b)
+print(teacher)
