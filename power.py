9491a3c8176a6eb9158fc2161361fb44e33d59fb
Prerna5678 <2002jain2811@gmail.com>
2025-08-21 19:35:01 +0530

    Python

--- power.py
+++ power.py
@@ -3,4 +3,4 @@ def power(x, y):
         return 1 
     else:
         return x * power(x, y - 1)
-print(power(2, 3)) 
+print(power(2, 5)) 
--- power_recus.py
+++ power_recus.py
@@ -1,5 +1,7 @@
 def power(x, y):
     result = 1
-    for i in range(x, y + 1):
+    for i in range( y+1):
+        print(i)
         result *= x
     return result
+print(power(2,5))
