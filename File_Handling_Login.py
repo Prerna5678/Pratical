fa44b228e187d259fe97a10c3f24087106a0a302
Prerna5678 <2002jain2811@gmail.com>
2025-09-01 12:54:35 +0530

    File Handling

---
+++ File_Handling_Login.py
@@ -0,0 +1,6 @@
+from File_Handling_Registration import *
+
+# Login form for Customer
+username = input("Enter your name: ")
+email = input("Enter your email: ")
+Cust_login = {'username': username, 'email': email}
